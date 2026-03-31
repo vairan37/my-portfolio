538f87dff9245fe94d1afcb7a03baa4e7da2c429
victor <vicraveau@gmail.com>
2026-04-01 01:26:45 +0200

    feat: scroll fluid

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/layout/theme-provider";
-
+import { ReactLenis } from 'lenis/react'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,9 +43,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <Analytics />
-          <SpeedInsights />
+          <ReactLenis root>
+            {children}
+            <Analytics />
+            <SpeedInsights />
+          </ReactLenis>
         </ThemeProvider>
       </body>
     </html>
